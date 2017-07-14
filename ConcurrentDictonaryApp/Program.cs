24b172c08e96b492c01771707906bf5f92527991
Atul Chaudhary <atul221282@gmail.com>
2017-07-15 00:22:56 +0930

    created console app

---
+++ ConcurrentDictonaryApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace ConcurrentDictonaryApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var mc = new MyCache();
+
+            Console.WriteLine(mc.GetOrAdd(1));
+
+            Console.WriteLine(mc.GetOrAdd(2));
+
+            Console.ReadKey();
+        }
+    }
+
+    public class MyCache
+    {
+        private readonly ConcurrentDictionary<int, string> cache;
+
+        public MyCache()
+        {
+            this.cache = new ConcurrentDictionary<int, string>();
+            this.cache.TryAdd(2, "Kapil");
+        }
+
+        public string GetOrAdd(int id)
+        {
+            return this.cache.GetOrAdd(id, "Atul");
+        }
+    }
+}
